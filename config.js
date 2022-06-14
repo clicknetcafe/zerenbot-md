6e35f9c5bca78a88e1265fc7781486ac95b9e7f4
N²O`eJuice <106494155+RYSZABOTZ@users.noreply.github.com>
2022-06-14 17:12:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -20,15 +20,15 @@ global.APIKeys = {
 // Ganti apikey lolhuman dan imgbbkey punya lu
 global.LolApiKey = 'hc8fyhce33ncq34uif'
 global.Imgbb = '7b2fde1f55b9a594d5db403f0a0c2712'
-global.owner = ['6282337245566']
-global.premium = ['6282337245566']
+global.owner = ['6282232684950']
+global.premium = ['6282232684950']
 global.packname = 'BunnyWalker'
 global.author = 'Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['!','.']
 global.sp = '⭔'
 // Ganti grup WA lu kalo pengen
-global.gwa = 'https://chat.whatsapp.com/LyPlkhIyMwPLnAM43nBCYR'
+global.gwa = 'https://chat.whatsapp.com/DtYKbAIrjx00WvIobmWTaa'
 global.mess = {
     success: '✓ Success',
     admin: 'Fitur Khusus Admin Group!',
